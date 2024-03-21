1319c4e2fcb08160fb6ff03da1b99f14916fb298
Moussa Fall <fallmoussa98xo@gmail.com>
2024-03-21 17:01:02 +0100

    feat: generate qrcode from logged-user infos

--- WebClient/app/src/pages/ScanQRCode.js
+++ WebClient/app/src/pages/ScanQRCode.js
@@ -10,26 +10,27 @@ const ScanQRCode = (props) => {
 
     const [socketData, setSocketData] = useState([]);
     const [isMobileDeviceConnected, SetIsMobileDeviceConnected] = useState(false);
-    
+
     const socket = props.socket;
-    
+
     let location = useLocation();
     let navigate = useNavigate();
 
     const serverUrl = process.env.REACT_APP_SERVER_URL;
 
-    let connectedUser = location.state.user; 
+    let connectedUser = location.state.user;
+
 
 
     let valueQRcode = ''
-    if (connectedUser){
-        valueQRcode = connectedUser.user_id + ";" + connectedUser.first_name + ";" + connectedUser.last_name + ";"+ connectedUser.email + ";" + serverUrl
+    if (connectedUser) {
+        valueQRcode = connectedUser.id + ";" + connectedUser.prenom + ";" + connectedUser.nom + ";" + connectedUser.email + ";" + serverUrl
     }
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        
-        const handleMobileDeviceConnected = (data) =>{
+
+        const handleMobileDeviceConnected = (data) => {
             setSocketData(data);
             SetIsMobileDeviceConnected(true);
             console.log(data);
@@ -39,74 +40,74 @@ const ScanQRCode = (props) => {
         socket.on('mobile_device_connected', handleMobileDeviceConnected)
 
     }, [socketData, socket])
-  return (
-    <div>
-        <Navbar />
-        <Container>
-            <Grid
-                container
-                spacing={0}
-                direction="column"
-                alignItems="center"
-                justifyContent="center"
-                sx={{ minHeight: '100vh' }}
+    return (
+        <div>
+            <Navbar />
+            <Container>
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    sx={{ minHeight: '100vh' }}
                 >
-                <Grid item xs={3}>
-                <Typography variant='h5'>Connecter votre appareil mobile</Typography>
-                <br/>
-            
-                <Card>
-                    <CardContent>
-                        {connectedUser && (
-                            <>
-                                {isMobileDeviceConnected && (
-                                    <Alert variant="filled" severity="success" style={{marginBottom: '10px'}}>Appareil mobile connecté avec succès</Alert>
-                                )}
-                                <Typography variant='h6'>Utilisateur connecté: </Typography>
-                                <List>
-                                <ListItem>
-                                <ListItemText
-                                    primary={"ID utilisateur: " + connectedUser.user_id}
-                                />
-                                </ListItem>
-                                <ListItem>
-                                <ListItemText
-                                    primary={"Prénom et nom: " + connectedUser.first_name + " " + connectedUser.last_name }
-                                />
-                                </ListItem>
-                                <ListItem>
-                                <ListItemText
-                                    primary={"Email: " + connectedUser.email }
-                                />
-                                </ListItem>
-                                </List>
+                    <Grid item xs={3}>
+                        <Typography variant='h5'>Connecter votre appareil mobile</Typography>
+                        <br />
 
-                                {!isMobileDeviceConnected ? (
-                                    <Box style={{ background: 'white', padding: '16px', textAlign: 'center' }}>
-                                        <p style={{fontSize: '16px', fontWeight: 'bold', marginBottom: '5px'}}>Scanner le QR code.</p>
-                                        <QRCode value={valueQRcode} size={200}/>
-                                    </Box>
-                                ):(
-                                    <Box m={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
-                                        <Button variant="contained" color="success" sx={{ height: 40 }}
-                                            onClick={()=>navigate('/check-devices')}
-                                        >
-                                            Suivant
-                                        </Button>
-                                    </Box>
+                        <Card>
+                            <CardContent>
+                                {connectedUser && (
+                                    <>
+                                        {isMobileDeviceConnected && (
+                                            <Alert variant="filled" severity="success" style={{ marginBottom: '10px' }}>Appareil mobile connecté avec succès</Alert>
+                                        )}
+                                        <Typography variant='h6'>Utilisateur connecté: </Typography>
+                                        <List>
+                                            <ListItem>
+                                                <ListItemText
+                                                    primary={"ID utilisateur: " + connectedUser.id}
+                                                />
+                                            </ListItem>
+                                            <ListItem>
+                                                <ListItemText
+                                                    primary={"Prénom et nom: " + connectedUser.prenom + " " + connectedUser.nom}
+                                                />
+                                            </ListItem>
+                                            <ListItem>
+                                                <ListItemText
+                                                    primary={"Email: " + connectedUser.email}
+                                                />
+                                            </ListItem>
+                                        </List>
+
+                                        {!isMobileDeviceConnected ? (
+                                            <Box style={{ background: 'white', padding: '16px', textAlign: 'center' }}>
+                                                <p style={{ fontSize: '16px', fontWeight: 'bold', marginBottom: '5px' }}>Scanner le QR code.</p>
+                                                <QRCode value={valueQRcode} size={200} />
+                                            </Box>
+                                        ) : (
+                                            <Box m={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
+                                                <Button variant="contained" color="success" sx={{ height: 40 }}
+                                                    onClick={() => navigate('/check-devices')}
+                                                >
+                                                    Suivant
+                                                </Button>
+                                            </Box>
+                                        )}
+
+
+                                    </>
                                 )}
 
-                                
-                            </>
-                        )}
-                        
-                    </CardContent>
-                </Card>
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Container>
-    </div>
-  )
+            </Container>
+        </div>
+    )
 }
 
 export default ScanQRCode
